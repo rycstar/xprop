1a29def0403109803bfdde533b60188adb8e3e32
hh1307 <yinghao.xing@fanvil.com>
2020-11-25 14:03:00 +0800

    [XYH]replace TOOLCHAIN_REL with STRIP

--- image.mk
+++ image.mk
@@ -14,13 +14,13 @@ endif
 
 	if [ -f $(FV_APP_PATH)/x_prop/xprop_srv ]; then \
 		cp $(FV_APP_PATH)/x_prop/xprop_srv $(IMAGE_INSTALL_DIR)/rootfs/bin/xprop_srv ; \
-		$(TOOLCHAIN_REL)strip --strip-unneeded $(IMAGE_INSTALL_DIR)/rootfs/bin/xprop_srv; \
+		$(STRIP) --strip-unneeded $(IMAGE_INSTALL_DIR)/rootfs/bin/xprop_srv; \
 	fi;
 	if [ -f $(FV_APP_PATH)/x_prop/prop_get ]; then \
 		cp $(FV_APP_PATH)/x_prop/prop_get $(IMAGE_INSTALL_DIR)/rootfs/bin/prop_get ; \
-		$(TOOLCHAIN_REL)strip --strip-unneeded $(IMAGE_INSTALL_DIR)/rootfs/bin/prop_get; \
+		$(STRIP) --strip-unneeded $(IMAGE_INSTALL_DIR)/rootfs/bin/prop_get; \
 	fi;
 	if [ -f $(FV_APP_PATH)/x_prop/prop_set ]; then \
 		cp $(FV_APP_PATH)/x_prop/prop_set $(IMAGE_INSTALL_DIR)/rootfs/bin/prop_set ; \
-		$(TOOLCHAIN_REL)strip --strip-unneeded $(IMAGE_INSTALL_DIR)/rootfs/bin/prop_set; \
+		$(STRIP) --strip-unneeded $(IMAGE_INSTALL_DIR)/rootfs/bin/prop_set; \
 	fi;
